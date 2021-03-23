92701252bc4fd357b967de3fe36de6081d8084f7
pablonolasco <pablo.nolasco@live.com.mx>
2021-03-23 13:51:28 -0600

    paquetes

---
+++ fundamentos/12Paquetes/src/mx/com/globalmentorie/Utilerias.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.globalmentorie;
+
+/**
+ *
+ * @author Windows10
+ */
+public class Utilerias {
+  public static void impimir(String s){
+      System.out.println("s-"+s);
+  }  
+}
---
+++ fundamentos/12Paquetes/src/test/Test.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+//import mx.com.globalmentorie.Utilerias;
+import static mx.com.globalmentorie.Utilerias.impimir;
+/**
+ *
+ * @author Windows10
+ */
+public class Test {
+    public static void main(String[] args) {
+       // Utilerias.impimir("hola");
+        impimir("hola mundo");
+    }
+}
